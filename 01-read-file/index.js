8ef7140a1053f29f14b5447fda8d28d162772c2a
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2021-11-07 18:49:07 +0200

    refactor: changed code for better visible comprehension

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,5 +2,6 @@ const fs = require("fs");
 
 const readableStream = fs.createReadStream("01-read-file/text.txt", "utf8");
 readableStream.on("data", function (text) {
-  console.log(text);
+  console.log(`↓↓↓ В данном файле хранится следующий текст: ↓↓↓
+"${text}"`);
 });
