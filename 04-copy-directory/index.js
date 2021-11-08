87b872caf56baff1b7e0303c289c6e2b4d7b07f7
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2021-11-08 21:18:00 +0200

    fix: add update-option to delete old files before copying

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,31 +3,75 @@ const fsPromises = require("fs").promises;
 const path = require("path");
 
 const copyDir = (() =>
-  fs.readdir(`${path.dirname(__filename)}/files`, function (error, files) {
-    if (error) error;
-    else
-      fs.mkdir(
+  fsPromises
+    .mkdir(
+      `${path.dirname(__filename)}/files-copy`,
+      { recursive: true },
+      (err) => {
+        if (err) throw err;
+        else
+          console.log(
+            `
+---------------------------------------------------------
+* Folder "files-copy" was created/updated successfully! *
+---------------------------------------------------------`
+          );
+      }
+    )
+    .then(function () {
+      console.log(
+        `
+---------------------------------------------------------
+* Folder "files-copy" was created/updated successfully! *
+---------------------------------------------------------`
+      );
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+      fs.readdir(
         `${path.dirname(__filename)}/files-copy`,
-        { recursive: true },
-        (err) => {
-          if (err) throw err;
+        function (error, oldfiles) {
+          if (error) error;
+          else if (!error && oldfiles.length === 0) return;
           else
-            console.log(
-              `Folder "files-copy" was created/updated successfully!`
+            oldfiles.forEach((oldfile) =>
+              fs.unlink(
+                `${path.join(__dirname, "./files-copy", oldfile)}`,
+                (err) => {
+                  if (err) throw err; // не удалось удалить файл
+                }
+              )
             );
         }
-      ) +
-        files.forEach((file) =>
-          fsPromises
-            .copyFile(
-              `${path.join(__dirname, "./files", file)}`,
-              `${path.join(__dirname, "./files-copy", file)}`
-            )
-            .then(function () {
-              console.log(`File "${file}" was copied to "files-copy"-folder!`);
-            })
-            .catch(function (error) {
-              console.log(error);
-            })
-        );
-  }))();
+      );
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+      fs.readdir(
+        `${path.dirname(__filename)}/files`,
+        function (error, newfiles) {
+          if (error) error;
+          else
+            newfiles.forEach((newfile) =>
+              fsPromises
+                .copyFile(
+                  `${path.join(__dirname, "./files", newfile)}`,
+                  `${path.join(__dirname, "./files-copy", newfile)}`
+                )
+                .then(function () {
+                  console.log(
+                    `File "${newfile}" was copied to the "files-copy"-folder!
+                    `
+                  );
+                })
+                .catch(function (error) {
+                  console.log(error);
+                })
+            );
+        }
+      );
+    }))();
